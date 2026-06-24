fef8c97b110f3e35317061d02a6355717f97421e
Amitkm997 <amitkm997@gmail.com>
2026-06-24 17:35:51 +0530

    arrays & objects

---
+++ Javascript/arrays/index.js
@@ -0,0 +1,94 @@
+
+// let num1=45;
+// let num2=89;
+// let num3=67;
+// let num4=64;
+
+// console.log(num3);
+
+// CRUD -> create, read , update, delete
+
+// let arr=[45,89,67,64,"Amit",[89,23]]; // creating an array
+
+// console.log(arr[4][2])
+
+// let twoDarray=[
+//     [23,24,25],
+//     [26,27,28],
+//     [29,30,31,[23,34,36,37]]
+// ]
+
+
+// console.log(arr[3]);//64
+// console.log(twoDarray[0][1])
+// console.log(twoDarray[2][3][1])
+
+
+// twoDarray[1][1]=72;
+// console.log(twoDarray)
+
+
+
+// let arr=[34,54,75,34,97,23]
+
+// arr.push(90); // add a element(90) at the end
+// arr.pop(); //removes a element(90) from the end
+// arr.shift();//removes a element(34) from the start
+// arr.unshift(12)//add a element(12) at the start
+
+// console.log(arr)//
+
+// let arr=[34,54,75,34,97,23]
+// let max=Infinity;//97
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]<max){
+//         max=arr[i];
+//     }
+// }
+
+// console.log(max)
+
+// let arr=[34,54,75,34,97,23]
+// swap1- [23,54,75,34,97,34]
+// swap2- [23,97,75,34,54,34]
+// output-> [23,97,34,75,54,34]
+
+// let result=[]
+
+// for(let i=arr.length-1;i>=0;i--){
+//     result.push(arr[i]);
+// }
+
+
+// let arr=[34,54,75,34,97,23]
+// let start=0;
+// let end=arr.length-1;
+
+// while(start<end){
+//      let temp=arr[start];
+//      arr[start]=arr[end];
+//      arr[end]=temp;
+//      start++;
+//      end--;
+// }
+// console.log(arr)
+
+
+// objects 
+
+let obj={
+    id:1,
+    name:"Amit",
+    occupation:"Trainer"
+}
+
+// console.log(obj.name)
+
+// console.log(obj['id']);
+
+obj.name="Naina"
+obj.studentName="Vishal"
+
+delete obj.id
+
+console.log(obj)
